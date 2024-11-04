b3a118ed27b79a20c85a8cb633d6936b9079900f
Tomato0O <183370587+Tomato0O@users.noreply.github.com>
2024-11-04 22:12:06 +0800

    ✔ complete index page layout

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,49 +1,26 @@
-// index.js
-const defaultAvatarUrl = 'https://mmbiz.qpic.cn/mmbiz/icTdbqWNOwNRna42FI242Lcia07jQodd2FJGIYQfG0LAJGFxM4FbnQP6yfMxBgJ0F3YRqJCJ1aPAK2dQagdusBZg/0'
-
 Page({
-  data: {
-    motto: 'Hello World',
-    userInfo: {
-      avatarUrl: defaultAvatarUrl,
-      nickName: '',
-    },
-    hasUserInfo: false,
-    canIUseGetUserProfile: wx.canIUse('getUserProfile'),
-    canIUseNicknameComp: wx.canIUse('input.type.nickname'),
-  },
-  bindViewTap() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onChooseAvatar(e) {
-    const { avatarUrl } = e.detail
-    const { nickName } = this.data.userInfo
-    this.setData({
-      "userInfo.avatarUrl": avatarUrl,
-      hasUserInfo: nickName && avatarUrl && avatarUrl !== defaultAvatarUrl,
-    })
-  },
-  onInputChange(e) {
-    const nickName = e.detail.value
-    const { avatarUrl } = this.data.userInfo
-    this.setData({
-      "userInfo.nickName": nickName,
-      hasUserInfo: nickName && avatarUrl && avatarUrl !== defaultAvatarUrl,
-    })
-  },
-  getUserProfile(e) {
-    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
-    wx.getUserProfile({
-      desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
-      success: (res) => {
-        console.log(res)
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    })
-  },
-})
+	// Page生命周期-页面加载中执行代码块
+	onLoad() {
+		// 使用 wx.getStorageSync 来获取存储的值
+		const hasUserLogin = wx.getStorageSync("hasUserLogin");
+		// console.log(hasUserLogin);
+
+		if (hasUserLogin) {
+			// 如果用户已登录，跳转到 main 页面
+			wx.redirectTo({
+				url: '/pages/main/main',
+			});
+		}
+	},
+
+	wx_login() {
+		wx.getUserProfile({
+			desc: '获取权限用于展示用户信息',
+			success: (res) => {
+				// console.log(res);
+				// 设置用户登录状态为 true
+				wx.setStorageSync("hasUserLogin", true);
+			},
+		});
+	}
+});
