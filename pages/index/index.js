a84be85486ee5ad7e4716f02142b02d917c5c7be
Tomato0O <183370587+Tomato0O@users.noreply.github.com>
2024-11-05 08:20:02 +0800

    ❗ fix index route

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,13 +1,16 @@
 Page({
+	data: {
+		hasUserLogin: false
+	},
 	// Page生命周期-页面加载中执行代码块
 	onLoad() {
 		// 使用 wx.getStorageSync 来获取存储的值
 		const hasUserLogin = wx.getStorageSync("hasUserLogin");
 		// console.log(hasUserLogin);
-
 		if (hasUserLogin) {
 			// 如果用户已登录，跳转到 main 页面
-			wx.redirectTo({
+			// 这里不能用wxwx.redirectTo的原因是tabbar页面不能作为重定向的url，所以用relaunch来替换
+			wx.reLaunch({
 				url: '/pages/main/main',
 			});
 		}
@@ -20,6 +23,9 @@ Page({
 				// console.log(res);
 				// 设置用户登录状态为 true
 				wx.setStorageSync("hasUserLogin", true);
+				wx.reLaunch({
+					url: '/pages/main/main',
+				})
 			},
 		});
 	}
