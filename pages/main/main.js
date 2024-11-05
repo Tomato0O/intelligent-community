49ab20bce6aafa107516ba5ab0696da3bccd6caa
Tomato0O <183370587+Tomato0O@users.noreply.github.com>
2024-11-05 14:50:55 +0800

    ➖ Remove custom-tabbar, use <app.json> instead.

--- pages/main/main.js
+++ pages/main/main.js
@@ -1,66 +1,27 @@
 // pages/main/main.js
+var images_list;
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad(options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
-  }
+	data: {
+		images_list: []
+	},
+	onLoad(e) {
+		wx.request({
+			// url: 'http://localhost:8080/getImages',
+			url: 'https://www.baidu.com',
+			success: (res) => {
+				this.setData({
+					// images_list: res.data,
+					images_list: [
+						"https://imgapi.jinghuashang.cn/random",
+						"https://imgapi.jinghuashang.cn/random",
+						"https://imgapi.jinghuashang.cn/random"
+					]
+				})
+			},
+			fail(e) {
+				console.log(e);
+			},
+		})
+	}
 })
