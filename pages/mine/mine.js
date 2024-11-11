b0d77bd3cc75772bb2a8a75b64c0b671d39948ee
Tomato0O <183370587+Tomato0O@users.noreply.github.com>
2024-11-11 19:23:33 +0800

    ➕ Complete Mine Page Structure.

--- pages/mine/mine.js
+++ pages/mine/mine.js
@@ -1,37 +1,68 @@
 // pages/mine/mine.js
 Page({
-	msg(){
-		console.log(1);
-	},
 	/**
 	 * 页面的初始数据
 	 */
 	data: {
 		nickName: "",
 		avatarUrl: "",
-		middlewareList: [{
-			// 在线svg图标库：https://iconpark.oceanengine.com/official，测试页面布局用
-			"iconUrl": "/images/mine/mail.svg",
-			"text": "我的消息",
-			"tap":"msg"
-		}, {
-			"iconUrl": "/images/mine/setting.svg",
-			"text": "个人资料"
-		}, {
-			"iconUrl": "/images/mine/edit.svg",
-			"text": "个人资料"
-		}, {
-			"iconUrl": "/images/mine/documents.svg",
-			"text": "jqk"
-		}, {
-			"iconUrl": "/images/mine/check.svg",
-			"text": "check"
-		}, {
-			"iconUrl": "/images/mine/aiming.svg",
-			"text": "jqk"
-		}, ],
+		// 中间的【物业服务】模块
+		serviceItemsList: [{
+				// tapMethod:点击模块后跳转到什么方法
+				"tapMethod": "pending",
+				"iconPath": "/images/mine/searching.svg",
+				"serviceName": "待处理"
+			},
+			{
+				"tapMethod": "",
+				"iconPath": "/images/mine/restart.svg",
+				"serviceName": "处理中"
+			},
+			{
+				"tapMethod": "",
+				"iconPath": "/images/mine/checked.svg",
+				"serviceName": "已完成"
+			},
+			{
+				"tapMethod": "",
+				"iconPath": "/images/mine/documents.svg",
+				"serviceName": "全部"
+			},
+		],
+		moreServicesList: [{
+				"iconPath": "/images/mine/messaging.svg",
+				"serviceName": "我的消息"
+			},
+			{
+				"iconPath": "/images/mine/ideal.svg",
+				"serviceName": "问题上报"
+			},
+			{
+				"iconPath": "/images/mine/checked.svg",
+				"serviceName": "已完成"
+			},
+			{
+				"iconPath": "/images/mine/checked.svg",
+				"serviceName": "已完成"
+			},
+			{
+				"iconPath": "/images/mine/checked.svg",
+				"serviceName": "已完成"
+			},
+			{
+				"iconPath": "/images/mine/checked.svg",
+				"serviceName": "已完成"
+			},
+			{
+				"iconPath": "/images/mine/checked.svg",
+				"serviceName": "已完成"
+			},
+			{
+				"iconPath": "/images/mine/setting.svg",
+				"serviceName": "个人设置"
+			},
+		]
 	},
-
 	/**
 	 * 生命周期函数--监听页面加载
 	 */
@@ -42,55 +73,5 @@ Page({
 			nickName: userInfo.nickName,
 			avatarUrl: userInfo.avatarUrl
 		})
-		// console.log(userInfo);
 	},
-
-	/**
-	 * 生命周期函数--监听页面初次渲染完成
-	 */
-	onReady() {
-
-	},
-
-	/**
-	 * 生命周期函数--监听页面显示
-	 */
-	onShow() {
-
-	},
-
-	/**
-	 * 生命周期函数--监听页面隐藏
-	 */
-	onHide() {
-
-	},
-
-	/**
-	 * 生命周期函数--监听页面卸载
-	 */
-	onUnload() {
-
-	},
-
-	/**
-	 * 页面相关事件处理函数--监听用户下拉动作
-	 */
-	onPullDownRefresh() {
-
-	},
-
-	/**
-	 * 页面上拉触底事件的处理函数
-	 */
-	onReachBottom() {
-
-	},
-
-	/**
-	 * 用户点击右上角分享
-	 */
-	onShareAppMessage() {
-
-	}
 })
